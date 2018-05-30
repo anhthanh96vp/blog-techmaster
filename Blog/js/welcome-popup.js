84319ff54ed2ecbf23cef78d777c1fc8966b0d5e
Thanh Na <anhthanh1996vp@gmail.com>
2018-05-31 02:19:54 +0700

    night commit

--- Blog/js/welcome-popup.js
+++ Blog/js/welcome-popup.js
@@ -1,19 +1,19 @@
-// var welcomePopup = document.getElementById("welcome-popup");
-// var welcomeClose = document.querySelector(".welcome__close");
+var welcomePopup = document.getElementById("welcome-popup");
+var welcomeClose = document.querySelector(".welcome__close");
 
-// welcomeClose.addEventListener("click", closePopup);
-// window.addEventListener("click", outsidePopup);
+welcomeClose.addEventListener("click", closePopup);
+window.addEventListener("click", outsidePopup);
 
-// setTimeout(function() {
-//   welcomePopup.style.display = "block";
-// }, 3000);
+setTimeout(function() {
+  welcomePopup.style.display = "block";
+}, 3000);
 
-// function closePopup() {
-//   welcomePopup.style.display = "none";
-// }
+function closePopup() {
+  welcomePopup.style.display = "none";
+}
 
-// function outsidePopup(e) {
-//   if (e.target == welcomePopup) {
-//     welcomePopup.style.display = "none";
-//   }
-// }
+function outsidePopup(e) {
+  if (e.target == welcomePopup) {
+    welcomePopup.style.display = "none";
+  }
+}
