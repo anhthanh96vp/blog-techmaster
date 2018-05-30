2c70eeb20bdcc7519d87272ecc367de6c2526d54
Thành <anhthanh1996vp@gmail.com>
2018-05-30 18:51:48 +0700

    update
    update

---
+++ Blog/js/welcome-popup.js
@@ -0,0 +1,19 @@
+// var welcomePopup = document.getElementById("welcome-popup");
+// var welcomeClose = document.querySelector(".welcome__close");
+
+// welcomeClose.addEventListener("click", closePopup);
+// window.addEventListener("click", outsidePopup);
+
+// setTimeout(function() {
+//   welcomePopup.style.display = "block";
+// }, 3000);
+
+// function closePopup() {
+//   welcomePopup.style.display = "none";
+// }
+
+// function outsidePopup(e) {
+//   if (e.target == welcomePopup) {
+//     welcomePopup.style.display = "none";
+//   }
+// }
